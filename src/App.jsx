58b10e6d4ea74899542a24bd761d1527a7f9d62a
Dev <devmdjbv1234@gmail.com>
2024-12-09 14:31:53 +0530

    App Completed

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Logo from './Logo';
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
   { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
